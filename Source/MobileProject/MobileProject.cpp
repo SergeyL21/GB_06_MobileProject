f3d431121a40ac19f6d01bd26eb619a23d0a9485
Sergey Lola <sergey_lola@mail.ru>
2022-02-06 21:37:12 +0300

    Project converted to mobile version

---
+++ Source/MobileProject/MobileProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MobileProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobileProject, "MobileProject" );
