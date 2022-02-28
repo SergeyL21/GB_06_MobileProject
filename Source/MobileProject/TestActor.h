6b2ae7a367a3add61d76aec6ad0e2d6024770f3d
Sergey Lola <sergey_lola@mail.ru>
2022-03-01 00:37:09 +0300

    C++ realization for the lesson

--- Source/MobileProject/TestActor.h
+++ Source/MobileProject/TestActor.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "TestActor.generated.h"
 
+class UTexture;
+
 UCLASS()
 class MOBILEPROJECT_API ATestActor : public AActor
 {
@@ -15,11 +17,29 @@ public:
 	// Sets default values for this actor's properties
 	ATestActor();
 
+	UFUNCTION(BlueprintCallable)
+	void FlipFlopTexture();
+
 protected:
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent *StaticMeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	UTexture *DefaultStateTexture;
+
+	UPROPERTY(EditAnywhere)
+	UTexture *AltStateTexture;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY()
+	class UMaterialInstanceDynamic *MaterialInstanceDynamic;
+
+	bool bIsDefaultStateTexture {true};
 };
